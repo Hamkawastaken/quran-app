d9ec5f6b12394b02a56fc96b9891714e9c289120
Muhammad Hamka <hamkarifai49@gmail.com>
2025-08-14 10:55:28 +0700

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "./components/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-br from-[#020617] via-[#0f172a] to-[#064e3b] animate-gradient
+`}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,83 @@
-import Image from "next/image";
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Link from "next/link";
+
+interface SurahProps {
+  nomor: number;
+  nama: string;
+  namaLatin: string;
+  jumlahAyat: number;
+  tempatTurun: TempatTurun;
+  arti: string;
+  deskripsi: string;
+  audioFull: { [key: string]: string };
+}
+
+enum TempatTurun {
+  Madinah = "Madinah",
+  Mekah = "Mekah",
+}
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const { data, isLoading, isError } = useQuery<SurahProps[]>({
+    queryKey: ["daftar-surah"],
+    queryFn: async () => {
+      const response = await axios.get("https://equran.id/api/v2/surat");
+      return response.data.data;
+    },
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  if (isLoading) {
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    );
+  }
+
+  if (isError) {
+    return (
+      <>
+        <p>Error 404 Not Found</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <main className="container mx-auto p-4">
+        <div className="min-h-screen flex flex-col justify-center items-center p-4">
+          <h1 className="font-black text-5xl bg-gradient-to-r from-emerald-400 to-cyan-400 bg-clip-text text-transparent mt-24">
+            Al - Quran Digital
+          </h1>
+          <p className="mt-4 max-w-lg text-center text-slate-400">
+            Baca, dengarkan, dan pelajari Al-Qur&apos;an secara online kapan
+            saja dan di mana saja, disertai dengan terjemahan
+          </p>
+
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-auto mt-24">
+            {data?.map((surah) => (
+              <div className="flex items-center gap-8 bg-slate-800 p-4 rounded-lg border border-slate-700 cursor-pointer" key={surah.nomor}>
+                <div className="size-12 rounded-lg bg-slate-700 flex justify-center items-center font-bold text-center">
+                  {surah.nomor}
+                </div>
+
+                <div className="">
+                  <h4 className="font-bold text-lg w-48">{surah.namaLatin}</h4>
+                  <p className="text-slate-400 text-sm">{surah.arti}</p>
+                </div>
+
+                <div>
+                  <h4 className="font-bold text-lg">{surah.nama}</h4>
+                  <p className="text-slate-400 text-sm">{surah.jumlahAyat} Ayat</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
