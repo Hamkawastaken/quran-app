9e05ee05838cde80dd1379a6e0be737488e5bab0
Muhammad Hamka <hamkarifai49@gmail.com>
2025-08-14 14:01:18 +0700

    Memindahkan Kode Surah Card Ke Komponen Sendiri

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Link from "next/link";
+import { SurahCard } from "./components/SurahCard";
 
 interface SurahProps {
   nomor: number;
@@ -57,32 +58,7 @@ export default function Home() {
             saja dan di mana saja, disertai dengan terjemahan
           </p>
 
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-auto mt-24">
-            {data?.map((surah) => (
-              <div
-                className="flex items-center gap-8 bg-slate-800 p-4 rounded-lg border border-slate-700 cursor-pointer"
-                key={surah.nomor}
-              >
-                <div className="size-12 rounded-lg bg-slate-700 flex justify-center items-center font-bold text-center">
-                  {surah.nomor}
-                </div>
-
-                <div className="">
-                  <h4 className="font-bold text-lg w-48">{surah.namaLatin}</h4>
-                  <p className="text-slate-400 text-sm">{surah.arti}</p>
-                </div>
-
-                <div className="text-end">
-                  <h4 className="text-2xl font-amiri text-cyan-500">
-                    {surah.nama}
-                  </h4>
-                  <p className="text-slate-400 text-sm">
-                    {surah.jumlahAyat} Ayat
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
+          <SurahCard data={data ?? []} />
         </div>
       </main>
     </>
