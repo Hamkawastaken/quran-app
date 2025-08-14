6c831e8fa2021d068a325aa2169dcdda8f6e7eb7
Muhammad Hamka <hamkarifai49@gmail.com>
2025-08-14 15:04:09 +0700

    Merapikan Tampilan Mobile + Dekstop

--- src/app/components/SurahCard.tsx
+++ src/app/components/SurahCard.tsx
@@ -12,19 +12,23 @@ interface SurahCardProps {
 
 export const SurahCard = ({ data }: SurahCardProps) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-auto mt-24">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-auto mt-6 md:mt-24">
       {data?.map((surah) => (
         <div
-          className="flex items-center gap-8 bg-slate-800 p-4 rounded-lg border border-slate-700 cursor-pointer"
+          className="flex items-center justify-between gap-8 bg-slate-800 p-4 rounded-lg border border-slate-700 cursor-pointer"
           key={surah.nomor}
         >
-          <div className="size-12 rounded-lg bg-slate-700 flex justify-center items-center font-bold text-center">
-            {surah.nomor}
-          </div>
-
-          <div className="">
-            <h4 className="font-bold text-lg w-48">{surah.namaLatin}</h4>
-            <p className="text-slate-400 text-sm">{surah.arti}</p>
+          <div className="flex flex-row gap-x-4">
+            {" "}
+            <div className="size-12 rounded-lg bg-slate-700 flex justify-center items-center font-bold text-center">
+              {surah.nomor}
+            </div>
+            <div className="">
+              <h4 className="font-bold text-lg md:w-24 lg:w-48">
+                {surah.namaLatin}
+              </h4>
+              <p className="text-slate-400 text-sm">{surah.arti}</p>
+            </div>
           </div>
 
           <div className="text-end">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,10 +50,10 @@ export default function Home() {
     <>
       <main className="container mx-auto p-4">
         <div className="min-h-screen flex flex-col justify-center items-center p-4">
-          <h1 className="font-black text-5xl bg-gradient-to-r from-emerald-400 to-cyan-400 bg-clip-text text-transparent mt-24">
+          <h1 className="font-black text-2xl md:text-5xl bg-gradient-to-r from-emerald-400 to-cyan-400 bg-clip-text text-transparent mt-12 md:mt-24">
             Al - Quran Digital
           </h1>
-          <p className="mt-4 max-w-lg text-center text-slate-400">
+          <p className="mt-4 text-xs md:text-lg max-w-md md:max-w-lg text-center text-slate-400">
             Baca, dengarkan, dan pelajari Al-Qur&apos;an secara online kapan
             saja dan di mana saja, disertai dengan terjemahan
           </p>
