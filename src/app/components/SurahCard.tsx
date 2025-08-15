e27c2f133ce45fc0d759ad6a8f68a8ba6633d514
Muhammad Hamka <hamkarifai49@gmail.com>
2025-08-15 11:25:59 +0700

    menambahkan styling hover pada card surah

--- src/app/components/SurahCard.tsx
+++ src/app/components/SurahCard.tsx
@@ -1,4 +1,3 @@
-
 type Surah = {
   nomor: number;
   nama: string;
@@ -16,12 +15,12 @@ export const SurahCard = ({ data }: SurahCardProps) => {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto mt-6 md:mt-12">
       {data?.map((surah) => (
         <div
-          className="flex items-center justify-between gap-8 bg-slate-800 p-4 rounded-lg border border-slate-700 cursor-pointer"
+          className="flex items-center justify-between gap-8 bg-slate-800 p-4 rounded-lg border border-slate-700 cursor-pointer hover:bg-gradient-to-br from-slate-800 to-250% to-cyan-500 transition-all ease-in-out hover:border-cyan-700 group"
           key={surah.nomor}
         >
           <div className="flex flex-row gap-x-4">
             {" "}
-            <div className="size-12 rounded-lg bg-slate-700 flex justify-center items-center font-bold text-center">
+            <div className="size-12 rounded-lg bg-slate-700 group-hover:bg-cyan-500 group-hover:text-black flex justify-center items-center font-bold text-center">
               {surah.nomor}
             </div>
             <div className="">
