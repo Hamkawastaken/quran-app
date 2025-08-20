0fe39e61fbe3071febcdcba5f3cfbb4d7b7118f8
Muhammad Hamka <hamkarifai49@gmail.com>
2025-08-20 11:23:51 +0700

    merapikan tampilan halaman bookmark di mobile

--- src/app/bookmark/page.tsx
+++ src/app/bookmark/page.tsx
@@ -9,15 +9,18 @@ const BookmarkPage = () => {
       <div className="min-h-screen flex flex-col items-center justify-center p-4">
         <div className="w-64 mx-auto md:w-3xl">
           <div className="grid grid-cols-1 md:grid-cols-3">
-            <div className="flex items-center gap-2 md:mb-8 mb-4">
-              <ArrowLeft className="size-4" />
+            <Link
+              href={"/"}
+              className="flex items-center gap-2 md:mb-8 mb-4 group"
+            >
+              <ArrowLeft className="size-4 group-hover:text-cyan-500 transition-all" />
               <button
                 onClick={() => history.back()}
-                className="text-lg cursor-pointer"
+                className="hidden md:flex text-sm md:text-lg cursor-pointer group-hover:text-cyan-500 transition-all"
               >
                 Kembali
               </button>
-            </div>
+            </Link>
 
             {/* Title */}
             <div className="text-center mb-10">
@@ -32,16 +35,16 @@ const BookmarkPage = () => {
             <div className="flex justify-center mb-6">
               <Bookmark className="size-12" />
             </div>
-            <h2 className="text-lg font-semibold mb-2">
+            <h2 className="text-base md:text-lg font-semibold mb-2">
               Belum Ada Ayat Tersimpan
             </h2>
-            <p className="text-gray-400 text-sm mb-8">
+            <p className="text-gray-400 text-xs md:text-sm mb-8">
               Tandai ayat favorit Anda di halaman detail surah untuk
               menyimpannya di sini.
             </p>
             <Link
               href="/"
-              className="px-6 py-2 rounded-lg bg-teal-600 hover:bg-teal-700 transition text-white font-medium"
+              className="px-4 md:px-6 py-2 rounded-lg bg-teal-600 hover:bg-teal-700 transition text-white text-sm md:text-base font-medium"
             >
               Mulai Membaca
             </Link>
